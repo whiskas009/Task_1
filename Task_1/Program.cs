c199197e92a194ce3ceeb4f9ddacd7a489870aef
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-11 21:04:49 +0300

    1

---
+++ Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int speedMovement = 100;
+            short viewingRadius = 32;
+            double priceApple = 45.345;
+            float amper = 76.32f;
+            decimal volt = 21.3m;
+            long kilometers = 356743;
+            string studentName = "Daniil";
+            char firstLetterInName = 'A';
+            bool lightIsOn = true;
+            uint temperaturePointWater = 100;
+        }
+    }
+}
